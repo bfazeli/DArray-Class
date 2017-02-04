f3768bd74896c0b24254c75f0f627ea16d18b524
bijan102 <bfazeli1@gmail.com>
2017-02-04 15:13:08 -0800

    Implemented emptyArray and more1than0 fnxs

--- Project/Functions.cpp
+++ Project/Functions.cpp
@@ -6,12 +6,25 @@
 
 // Definition function emptyArray
 // Your code here...
-
+void DArray::emptyArray()
+{
+	numOfElements = 0;
+}
 
 
 // Definition function more1than0
 // Your code here...
+bool DArray::more1than0()
+{
+	int countOnes = 0, countZeroes = 0;
+	for (size_t i = 0; i < numOfElements; ++i)
+	{
+		if (a[i] == 0) ++countOnes;
+		if (a[i] == 1) ++countZeroes;
+	}
 
+	return countOnes > countZeroes;
+}
 
 
 // Definition function post4
